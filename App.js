e203cce89a4d1e863f9e9c6ebcf809a3cc776bcb
Joseph Akayesi <josephakayesi@gmail.com>
2018-08-23 09:09:22 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+import {NativeRouter, Switch, Route} from 'react-router-native';
+
+import LandingPage from './screens/LandingPage';
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+
+
+export default class App extends Component {
+  render(){
+    return(      
+      <NativeRouter>
+        <Switch>
+          <Route exact path = "/" component = {LandingPage}/>
+          <Route exact path = "/login" component = {Login}/>   
+          <Route exact path = "/signup" component = {SignUp}/>         
+        </Switch>
+      </NativeRouter>
+              
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('App', () => App);
---
+++ screens/SignUp.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { StyleSheet, View, TextInput, TouchableOpacity,Text, TouchableHighlight, ImageBackground } from 'react-native';
+
+
+export default ({history}) => (
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('./images/backdrop.png')}
+        style={styles.container}
+        imageStyle={{ resizeMode: 'cover' }}>
+
+        <View style={styles.boxOne}>
+  
+        </View>
+        <View style={styles.boxTwo}>
+            <TextInput
+                placeholder="username"
+                placeholderTextColor="red"
+                style={styles.input}>
+            </TextInput>
+            <TextInput
+                placeholder="password"
+                placeholderTextColor="red"
+                style={styles.input}>
+            </TextInput>
+            <TextInput
+                placeholder="email or telephone"
+                placeholderTextColor="red"
+                style={styles.input}>
+            </TextInput>
+            <TouchableOpacity style={styles.proceedButton}>
+                <Text style={styles.paragraph}>PROCEED</Text>
+            </TouchableOpacity>
+            <TouchableHighlight style={styles.alternativeProcedure} onPress={() => history.push("./Login")}>
+                <Text style={styles.alternativeProcedureText}>Login Instead ?</Text>
+            </TouchableHighlight>
+        </View>
+      </ImageBackground>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#B6B6B6'
+    },
+    boxOne:{
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    boxTwo: {
+        padding: 20,
+    },
+    input: {
+        height: 40,
+        backgroundColor: "white",
+        marginBottom: 20,
+        color: "skyblue",
+        paddingHorizontal: 10
+    },
+    proceedButton: {
+        backgroundColor: '#ffffff',
+        padding: 20,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+        
+    },
+    paragraph: {
+        fontSize: 24,
+        fontWeight: 'bold',        
+    },
+
+    alternativeProcedure: {
+        alignItems: 'center',
+        bottom: -5,       
+    },
+
+    alternativeProcedureText: {
+      fontWeight: 'bold',
+    }
+});
